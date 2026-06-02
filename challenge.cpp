86bce63cdaaabef1c46fd920b06324150100e6a3
ahmadmufti1 <muftiahmad617@gmail.com>
2026-06-02 16:23:18 +0700

    first commit

---
+++ challenge.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <exception>
+#include <array>
+#include <string>
+#include <fstream> //untuk file I/O (Stream Reader/Writer)
+#include <vector> //untuk manipulasi data saat update dan delete
+
+using namespace std;
